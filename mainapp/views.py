8041c959f947968b583060a5f541aa9f05a90301
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2023-02-02 00:51:18 +0545

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,1 @@
-from django.shortcuts import render, redirect
-from .functions import createRoomToken, getmember
 
-# Create your views here.
-def create_token(request):
-    return createRoomToken(request)
-
-
-def getMember(request):
-    return getmember(request)
-
-def createOrJoinRoom(request):
-    context={}
-    return render(request, 'createRoom.html', context)
-
-def getintoRoom(request):
-    return render(request,'room.html',{})
