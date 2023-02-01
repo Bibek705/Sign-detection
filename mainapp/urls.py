d1c3e86fea86e48979f49ebaad5d1fddcd7c2215
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2023-02-02 00:49:58 +0545

    video calling only final backend

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import createOrJoinRoom, getintoRoom, get_token
+from .views import createOrJoinRoom, create_token, getintoRoom, getMember
 
 urlpatterns = [
     path('', createOrJoinRoom, name='roomCreate'),
     path('room/', getintoRoom, name='gotoRoom'),
-    path('getToken/', get_token, name='getToken'),
+    path('createToken/', create_token, name='createToken'),
+    path('get_member/', getMember, name='getmember'),
 ]
