666756a74bcb48ef0863bf64dce6cc224d2533f0
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2023-02-02 00:48:04 +0545

    video calling only final backend

--- mainapp/functions.py
+++ mainapp/functions.py
@@ -3,6 +3,7 @@ import time
 import string
 from django.http import JsonResponse
 from agora_token_builder import RtcTokenBuilder
+from .models import usersModel
 
 def createRoomID():
     letters = string.ascii_lowercase
@@ -17,14 +18,34 @@ def checkForRoom(id):
 '''
 
 def createRoomToken(request):
-    userName = request.GET.get('userName')
+    channel = request.GET.get('userName')
+    member = request.GET.get('room')
     appId = '1f3c5f4a522a49f6aa8e7b2b3b3c21a8'
     appCertificate='4a227d13fe624dfc9dfe3b473e6948e7'
-    channelName=createRoomID()
+    if channel=='':
+        while True:
+            channelName=createRoomID()
+            if usersModel.objects.filter(channel=channelName).count()==0:
+                break
+    else:
+        channelName=channel
     uid=random.randint(1,230)
     role=1
     expirationTimeInSec=3600*6
     currentTime=time.time()
     privilegeExpiredTs=currentTime+expirationTimeInSec
     token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
-    return JsonResponse({'token':token,'uid':uid,'channel':channelName ,'username':userName},safe=False)
+    usersModel.objects.create(channel=channelName, uid=uid, name=member, role=role)
+    return JsonResponse({'token':token,'uid':uid,'channel':channelName, 'user':member},safe=False)
+
+
+def getmember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = usersModel.objects.get(
+        uid=uid,
+        channel=room_name,
+    )
+    name = member.name
+    return JsonResponse({'name': member.name}, safe=False)
